e2e2f358f4662dc289ff90bae92194acd67c87c2
M Tayyab Saeed <tayyab.saeed05@gmail.com>
2018-07-27 19:39:54 +0200

    Added style to all the pages

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
+
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 DBsession = sessionmaker(bind = engine)
 session = DBsession()
 
 app = Flask(__name__)
+
 
 @app.route('/restaurants/')
 def restaurants():
@@ -19,16 +21,20 @@ def restaurants():
     else:
         return "Request couldn't Found"
 
+
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurant_menu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     menu_items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id)
     if menu_items:
-        return render_template('restaurantmenu.html', restaurant=restaurant, menu_items=menu_items)
+        return render_template('restaurantmenu.html',
+                               restaurant=restaurant, menu_items=menu_items)
     else:
         return "No Menus Found"
 
-@app.route('/restaurants/<int:restaurant_id>/create-menu-item/', methods=['GET', 'POST'])
+
+@app.route('/restaurants/<int:restaurant_id>/create-menu-item/',
+           methods=['GET', 'POST'])
 def create_menu_item(restaurant_id):
     if request.method == 'POST':
         new_menu_item = MenuItem(name=request.form['new_menu_item'],
@@ -44,7 +50,9 @@ def create_menu_item(restaurant_id):
     else:
         return render_template('createnewmenu.html', restaurant_id=restaurant_id)
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit-menu-item/', methods=['GET', 'POST'])
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit-menu-item/', 
+           methods=['GET', 'POST'])
 def edit_menu_item(restaurant_id, menu_id):
     menu_item_to_edit = session.query(MenuItem).filter_by(id=menu_id).one()
     if request.method == 'POST':
@@ -61,7 +69,9 @@ def edit_menu_item(restaurant_id, menu_id):
         return render_template('editmenuitem.html', restaurant_id=restaurant_id,
                                menu_item=menu_item_to_edit)
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete-menu-item/', methods=['GET', 'POST'])
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete-menu-item/', 
+           methods=['GET', 'POST'])
 def delete_menu_item(restaurant_id, menu_id):
     item_to_delete = session.query(MenuItem).filter_by(id=menu_id).one()
     if request.method == 'POST':
@@ -72,6 +82,8 @@ def delete_menu_item(restaurant_id, menu_id):
     else:
         return render_template('deletemenuitem.html', restaurant_id=restaurant_id, 
                                menu_item=item_to_delete)
+
+
 
 if __name__ == '__main__':
     app.secret_key = 'super-secret-key'
